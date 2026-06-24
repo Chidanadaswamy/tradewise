979e0a2bee6a3b7c361a4d1bbe5136fbdd8fc4b5
Chidananda Swamy H C <income.10000000@gmail.com>
2026-06-24 18:57:55 +0530

    Added first JUnit test

---
+++ src/test/java/com/platform/TradeWiseApplicationTests.java
@@ -0,0 +1,12 @@
+package com.seedling.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
